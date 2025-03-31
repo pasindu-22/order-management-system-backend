f1a3840ba93f85abbd6a1272fc8f51fd16009437
Pasindu Sathsara Rasanga <155545637+pasindu-22@users.noreply.github.com>
2025-03-31 08:43:27 +0530

    Implementation of Unique UserID generation

---
+++ user-service/src/main/java/com/ecommerce/user_service/service/SequenceGeneratorService.java
@@ -0,0 +1,32 @@
+package com.ecommerce.user_service.service;
+
+import com.ecommerce.user_service.model.DatabaseSequence;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.mongodb.core.MongoOperations;
+import org.springframework.data.mongodb.core.query.Criteria;
+import org.springframework.data.mongodb.core.query.Query;
+import org.springframework.data.mongodb.core.query.Update;
+import org.springframework.data.mongodb.core.FindAndModifyOptions;
+import org.springframework.stereotype.Service;
+
+@Service
+public class SequenceGeneratorService {
+    private final MongoOperations mongoOperations;
+
+    @Autowired
+    public SequenceGeneratorService(MongoOperations mongoOperations) {
+        this.mongoOperations = mongoOperations;
+    }
+
+    public String generateEmployeeId() {
+        String seqName = "employee_sequence";
+        DatabaseSequence counter = mongoOperations.findAndModify(
+                Query.query(Criteria.where("_id").is(seqName)),
+                new Update().inc("seq", 1),
+                FindAndModifyOptions.options().returnNew(true).upsert(true),
+                DatabaseSequence.class);
+
+        long sequenceNumber = counter.getSeq();
+        return String.format("EMP-%03d", sequenceNumber);
+    }
+}
